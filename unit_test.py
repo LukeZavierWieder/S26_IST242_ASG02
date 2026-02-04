8cd4ce95e7d9de5276d09585223ba1dc5d0c9a9e
bkm5912 <sam@lehighradio.com>
2026-02-04 17:14:47 -0500

    Added normal case test for add_book
    must check the information in the dictionary
    against the information entered from the user to
    make sure it got added

--- unit_test.py
+++ unit_test.py
@@ -1,3 +1,35 @@
 '''
 Testing for personal library.
 '''
+
+import unittest
+import personal_library
+from unittest.mock import patch
+from io import StringIO
+
+from personal_library import (
+    add_book,
+    remove_book,
+    update_book,
+    list_books,
+    search_book,
+)
+
+# Test class
+class TestPersonalLibrary(unittest.TestCase):
+    # Test case for add book function
+    def test_add_book_normal_case(self):
+        library = []
+        with patch("builtins.input", return_value = ["test", "test2", "test3"]):
+            add_book()
+        self.assertEqual(personal_library.library,
+            [{
+                "title": "test",
+                "author": "test2",
+                "year_published": "test3"
+            }]
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
